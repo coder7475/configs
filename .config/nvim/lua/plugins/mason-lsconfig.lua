61c285d63241f9d85c62a43f7f7e0354b681ca16
coder7475 <robiulhossain7475@gmail.com>
2025-05-14 21:14:16 +0600

    feat: Updated lazyvim

---
+++ .config/nvim/lua/plugins/mason-lsconfig.lua
@@ -0,0 +1,12 @@
+-- ~/.config/nvim/lua/plugins/mason-lspconfig.lua
+
+return {
+  {
+    "mason-org/mason.nvim",
+    version = "^1.0.0", -- or a specific 1.x version e.g., "1.11.0"
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    version = "^1.0.0", -- or a specific 1.x version e.g., "1.32.0"
+  },
+}
